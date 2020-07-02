5802821ee7cae5a72d6cbf5c4cf823c58580c967
Sergey Seroukhov <seroukhov@gmail.com>
2020-07-02 16:56:01 -0700

    Changed language for comments microservice

--- checkout.sh
+++ checkout.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-git clone git@github.com:pip-services-social/pip-services-comments-dart.git
+git clone git@github.com:pip-services-social/pip-services-comments-node.git
 
-git clone git@github.com:pip-services-social/pip-clients-comments-dart.git
+git clone git@github.com:pip-services-social/pip-clients-comments-node.git
